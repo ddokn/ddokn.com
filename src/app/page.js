61b3724c7252f2e7b9e08739cfacdf72cda1852c
김도근 <k.ddokn@gmail.com>
2024-04-30 15:59:38 +0900

    AOS init

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,31 @@
+'use client'
+import { useEffect } from 'react';
+
 import Image from "next/image";
+import AOS from "aos";
+import 'aos/dist/aos.css';
 
 export default function Home() {
+  useEffect(()=>{
+    AOS.init();
+  }, []);
+  
   return (
     <div>
-      <h1>Hello, ddokn world!!</h1>
-      <p>안녕하세요!!</p>
+      <h1
+        data-aos="fade-up"
+        data-aos-easing="ease-out"
+        data-aos-duration="100"
+        data-aos-once="true"
+        data-aos-delay="300"
+      >Hello, ddokn world!!</h1>
+      <p
+        data-aos="fade-up"
+        data-aos-easing="ease-out"
+        data-aos-duration="150"
+        data-aos-once="true"
+        data-aos-delay="450"
+      >안녕하세요!!</p>
     </div>
   );
 }
